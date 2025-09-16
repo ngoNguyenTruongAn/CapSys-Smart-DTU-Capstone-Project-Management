ee026bd8fb6a6437fb967d637c504ffff5ec145f
main <quangff296@gmail.com>
2025-09-16 12:37:53 +0700

    q

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import LecturersLayout from "./layouts/Lecturers/LecturersLayout";
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} /> */}
-      <Route path="/" element={<LecturersLayout />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/lecturer" element={<LecturersLayout />} />
       {/* <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/lecturer" element={<LecturerDashboard />} />
         <Route path="/student" element={<StudentDashboard />} /> */}
