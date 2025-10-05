2f7c6727773b434748cc003f36f7f30317a72f2e
main <quangff296@gmail.com>
2025-10-06 00:53:53 +0700

    6/10

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,8 @@ function App() {
         <Route path="quan-ly-do-an" element={<QuanLyDoAn />} />
         <Route path="quan-ly-tai-khoan" element={<QuanLyTaiKhoan />} />
       </Route>
-      {/* <Route path="/lecturer" element={<LecturersLayout />}></Route> */}
       <Route path="verification-code" element={<VerificationCode />} />
       <Route path="confirm-forgot" element={<ConfirmForgot />} />
-      {/* <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/lecturer" element={<LecturerDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} /> */}
-      {/* sau này thêm ProtectedRoute để check role */}
     </Routes>
   );
 }
